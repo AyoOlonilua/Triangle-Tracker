2f43213ddc302219e56c4ed4ff26626a6dce553a
Ayomipe <oloniluaayomipe@gmail.com>
2023-08-11 10:28:30 +0100

    create html file

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $("form#triangle").submit(function(e) {
+    let side1 = parseInt($("#input#first").val());
+    let side2 = parseInt($("#input#second").val());
+    let side3 = parseInt($("#input#third").val());
+
+    
+        if (side1 === side2 && side1 > 0 && side2 > 0 && side3 > 0 && side2 === side3 && (side1 + side2) >= side3) {
+            $("#sides").text("This is an Equilateral triangle");
+        } 
+        else if (side1 === side2 && side1 > 0 && side2 > 0 && side3 > 0 && (side1 + side2) >= side3 || side3 === side2 && side1 >0 & side2 > 0 && side3 > 0 && (side1 + side2) >= side3 || side1 === side3 && side1 > 0 & side2 > 0 & side3 > 0 && (side1 + side2) >= side3) {
+            $("#sides").text("This is an Isosceles triangle");
+       } 
+        else if (side1 != side2 & side1 > 0 && side2 > 0 & side3 > 0 & side2 != side3 & (side1 + side2) >= side3)  {
+            $("#sides").text("This is a Scalene triangle");
+       } 
+        else {
+            $("#sides").text("This is not a triangle");
+       }
+    
+    e.preventDefault();
+
+    })
+
+});
